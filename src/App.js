d0be61e3d8e3c08eff6763f0984c6366a8bc4a4e
Francesco Bertolino <eriol1977@hotmail.com>
2020-05-17 15:15:03 +0200

    big commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,123 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Fragment, useState } from 'react';
 import './App.css';
+import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users';
+import axios from 'axios';
+import Search from './components/users/Search';
+import { Alert } from './components/layout/Alert';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { About } from './components/pages/About';
+import User from './components/users/User';
 
-function App() {
+const App = () => {
+  const [users, setUsers] = useState([]);
+  const [user, setUser] = useState({});
+  const [repos, setRepos] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [alert, setAlert] = useState(null);
+
+  const authHeader = {
+    headers: {
+      Authorization:
+        'Basic ' +
+        btoa(
+          process.env.REACT_APP_GITHUB_CLIENT_ID +
+            ':' +
+            process.env.REACT_APP_GITHUB_CLIENT_SECRET
+        ),
+    },
+  };
+
+  // useEffect(async () => {
+  //   setLoading(true);
+  //   const res = await axios.get('https://api.github.com/users', authHeader);
+  //   setLoading(false);
+  //   setUsers(res.data);
+  //   // eslint-disable-next-line
+  // }, []);
+
+  const searchUsers = async (text) => {
+    setLoading(true);
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}`,
+      authHeader
+    );
+    setLoading(false);
+    setUsers(res.data.items);
+  };
+
+  const getUser = async (username) => {
+    setLoading(true);
+    const res = await axios.get(
+      `https://api.github.com/users/${username}`,
+      authHeader
+    );
+    setLoading(false);
+    setUser(res.data);
+  };
+
+  const getUserRepos = async (username) => {
+    setLoading(true);
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc`,
+      authHeader
+    );
+    setLoading(false);
+    setRepos(res.data);
+  };
+
+  const clearUsers = () => {
+    setLoading(false);
+    setUsers([]);
+  };
+
+  const showAlert = (msg, type) => {
+    setAlert({ msg, type });
+    setTimeout(() => setAlert(null), 3000);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className='App'>
+        <Navbar />
+        <div className='container'>
+          <Alert alert={alert} />
+          <Switch>
+            <Route
+              exact
+              path='/'
+              render={(props) => (
+                <Fragment>
+                  <Search
+                    searchUsers={searchUsers}
+                    clearUsers={clearUsers}
+                    showClear={users.length > 0}
+                    setAlert={showAlert}
+                  />
+                  <Users loading={loading} users={users} />
+                </Fragment>
+              )}
+            />
+            <Route exact path='/about' component={About} />
+            <Route
+              exact
+              path='/user/:login'
+              render={(props) => (
+                <User
+                  {...props}
+                  getUser={getUser}
+                  getUserRepos={getUserRepos}
+                  user={user}
+                  repos={repos}
+                  loading={loading}
+                />
+              )}
+            />
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
